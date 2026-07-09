aacabeea3897bcb0a62d60ecf4b2f2f56d8e0ea9
GurleenKaur0786 <gurleenkaur72542@gmail.com>
2026-07-10 01:45:30 +1000

    Day 2: Added authentication - Login page, AuthContext, JWT middleware, User model, Admin & Employee dashboards

--- EMS-Day1/frontend/src/App.jsx
+++ EMS-Day1/frontend/src/App.jsx
@@ -1,13 +1,29 @@
-
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 import './App.css'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import EmployeeDashboard from './pages/EmployeeDashboard';
 
 function App() {
   
 
   return (
-    <div className='text-3xl text-teal-500'>Hello World</div>
-  
+    <>
+    <ToastContainer />
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to = "/admin-dashboard"/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/admin-dashboard' element={<AdminDashboard/>}></Route>
+        <Route path='/employee-dashboard' element={<EmployeeDashboard/>}></Route>
+      </Routes>
+    
+    </BrowserRouter>
+   </>
   )
 }
 
---
+++ EMS-Day1/frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+
+const AdminDashboard = () => {
+  const navigate = useNavigate()
+  const {user,loading} = useAuth()
+  if(loading){
+    return <div>Loading....</div>
+  }
+  if(!user){
+    navigate('/login')
+  }
+  return (
+    <div>AdminDashboard {user.name}</div>
+  )
+}
+
+export default AdminDashboard
